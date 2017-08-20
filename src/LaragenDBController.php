f7987c42edd8a86a2e0492d0e1448298982477c0
Fathur Rahman <fathur@nazrol.tech>
2017-08-20 08:13:34 +0800

    update json structure and composer config

--- src/LaragenDBController.php
+++ src/LaragenDBController.php
@@ -14,8 +14,8 @@ class LaragenDBController extends Controller
 
     public function index()
     {
-        $db_name = Config::get('database.connections.'.Config::get('database.default').'.database');
-        $db_table_name = 'Tables_in_'.$db_name;
+        $db_name = Config::get('database.connections.' . Config::get('database.default') . '.database');
+        $db_table_name = 'Tables_in_' . $db_name;
 
         $db_structure = [];
         $tables = DB::select('SHOW TABLES');
@@ -25,17 +25,11 @@ class LaragenDBController extends Controller
 
             //get all table column
 
-            $table_info_columns = DB::select( DB::raw('SHOW COLUMNS FROM '.$table_name));
+            $table_info_columns = DB::select(DB::raw('SHOW COLUMNS FROM ' . $table_name));
 
-
-
-            foreach($table_info_columns as $column){
-                $col_name = $column->Field;
-                $col_type = $column->Type;
-                $db_structure[$table_name][$col_name] = $column;
+            foreach ($table_info_columns as $column) {
+                $db_structure[$table_name][] = $column;
             }
-
-
         }
 
         return response()->json($db_structure);
